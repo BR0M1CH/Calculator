9f35f5ba665a1f05c1051595404cc82cf976dcc9
Kirill <322panda322panda322@gmail.com>
2022-11-26 12:42:33 +0300

    Связал функции с оболочкой

--- Calculator_functions.py
+++ Calculator_functions.py
@@ -1,17 +1,17 @@
 import re
-
-helper = []
+from Calculator_window import get_exp
 
 def enter():
-    expression = str(input("Введите выражение: "))
+    expression = get_exp()
+    print("EXP0=" ,expression)
     expression = re.split(r"([0-9]i)+|(\W)?", expression)
     while "" in expression: expression.remove("")
     while None in expression: expression.remove(None)
     return(expression)
 
 
-def bracket_finder():
-    global expression
+def bracket_finder(expression1):
+    expression = [i for i in expression1]
     nums = expression.count("(")
     left_bracket = 0
     for i in range(nums):
@@ -20,60 +20,42 @@ def bracket_finder():
     return left_bracket, right_bracket
 
 
-def calc_func(op, index, iterable):
-    global expression, helper
-    if iterable is expression:
-        expression[index-1] = complex(expression[index-1])
-        expression[index+1] = complex(expression[index+1])
-        if op == "*":
-            expression[index] = (expression[index-1]) * (expression[index+1])
-            expression.pop(index+1)
-            expression.pop(index-1)
-        elif op == "/":
-            expression[index] = (expression[index-1]) / (expression[index+1])
-            expression.pop(index+1)
-            expression.pop(index-1)
-        elif op == "+":
-            expression[index] = (expression[index-1]) + (expression[index+1])
-            expression.pop(index+1)
-            expression.pop(index-1)
-        elif op == "-":
-            expression[index] = (expression[index-1]) - (expression[index+1])
-            expression.pop(index+1)
-            expression.pop(index-1)
-    elif iterable is helper:
-        helper[index-1] = complex(helper[index-1])
-        helper[index+1] = complex(helper[index+1])
-        if op == "*":
-            helper[index] = (helper[index-1]) * (helper[index+1])
-            helper.pop(index+1)
-            helper.pop(index-1)
-        elif op == "/":
-            helper[index] = round((helper[index-1]) / (helper[index+1]), 5)
-            helper.pop(index+1)
-            helper.pop(index-1)
-        elif op == "+":
-            helper[index] = (helper[index-1]) + (helper[index+1])
-            helper.pop(index+1)
-            helper.pop(index-1)
-        elif op == "-":
-            helper[index] = (helper[index-1]) - (helper[index+1])
-            helper.pop(index+1)
-            helper.pop(index-1)
+def calc_func(op, index, iterable1):
+    iterable = [i for i in iterable1]
+    iterable[index-1] = complex(iterable[index-1])
+    iterable[index+1] = complex(iterable[index+1])
+    if op == "*":
+        iterable[index] = (iterable[index-1]) * (iterable[index+1])
+        iterable.pop(index+1)
+        iterable.pop(index-1)
+    elif op == "/":
+        iterable[index] = (iterable[index-1]) / (iterable[index+1])
+        iterable.pop(index+1)
+        iterable.pop(index-1)
+    elif op == "+":
+        iterable[index] = (iterable[index-1]) + (iterable[index+1])
+        iterable.pop(index+1)
+        iterable.pop(index-1)
+    elif op == "-":
+        iterable[index] = (iterable[index-1]) - (iterable[index+1])
+        iterable.pop(index+1)
+        iterable.pop(index-1)
+    return(iterable)
             
 
-def bracket_calc(left_index, right_index):
-    global expression, helper
+def bracket_calc(left_index, right_index, expression1):
+    helper= []
+    expression = [i for i in expression1]
     if (right_index - left_index) > 2:
         for i in range(left_index+1, right_index):
             helper.append(expression[i])
         helper_index = index_in_dict("*", "/", helper)
         while helper_index != []:
-            calc_func(helper[helper_index[0]],helper_index[0],helper)
+            helper = calc_func(helper[helper_index[0]],helper_index[0],helper)
             helper_index.pop(0)
         helper_index = index_in_dict("+", "-", helper)
         while helper_index != []:
-            calc_func(helper[helper_index[0]],helper_index[0],helper)
+            helper = calc_func(helper[helper_index[0]],helper_index[0],helper)
             helper_index.pop(0)
         for i in range(right_index, left_index, -1):
             expression.pop(i)
@@ -82,18 +64,19 @@ def bracket_calc(left_index, right_index):
     else:
         expression.pop(right_index)
         expression.pop(left_index)
+    return expression
 
-
-def main_calc():
-    global expression
+def main_calc(expression1):
+    expression = [i for i in expression1]
     while "*" in expression or "/" in expression:
         helper_index = index_in_dict("*", "/", expression)
-        calc_func(expression[helper_index[0]], helper_index[0], expression)
+        expression = calc_func(expression[helper_index[0]], helper_index[0], expression)
         helper_index.pop(0)
     while "+" in expression or "-" in expression:
         helper_index = index_in_dict("+", "-", expression)
-        calc_func(expression[helper_index[0]], helper_index[0], expression)
+        expression = calc_func(expression[helper_index[0]], helper_index[0], expression)
         helper_index.pop(0)
+    return(expression)
 
 
 
@@ -106,8 +89,8 @@ def index_in_dict(op1, op2, iterable):                #Возвращает сп
     return helper_index
 
 
-def complex_converter():
-    global expression
+def complex_converter(expression1):
+    expression = [i for i in expression1]
     for i in range(len(expression)):
         if "i" in expression[i]:
             expression[i] = re.sub('i', 'j', expression[i]) 
@@ -115,21 +98,24 @@ def complex_converter():
                 expression[i-2]+="".join(expression[j])
             for j in range(i-1, i+1):
                 expression[j] = None
-    while None in expression:
-        expression.remove(None)
+    while "None" in expression:
+        expression.remove("None")
+    return(expression)
             
+
+
+
+
+
 if __name__ == "__main__":
     expression = enter()
-    complex_converter()
+    expression = complex_converter(expression)
     while "(" in expression:
-        left_index, right_index = bracket_finder()
-        bracket_calc(left_index, right_index)
-    while len(expression) > 2:
-        main_calc()
-    if expression[0].imag == 0j:
-        print(expression[0].real)
-    else:
-        print(expression[0])
+        left_bracket, right_bracket = bracket_finder(expression)
+        expression = bracket_calc(left_bracket, right_bracket, expression)
+    while len(expression)>1:
+        expression = main_calc(expression)
+    print(expression)
 
 
 
